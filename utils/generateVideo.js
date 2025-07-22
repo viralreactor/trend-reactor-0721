0e37bc89e60006d710f35bb4de529bbbec9e5587
Viral Reactor <studiosenseai@gmail.com>
2025-07-21 20:08:48 -0400

    Update generateVideo.js

--- utils/generateVideo.js
+++ utils/generateVideo.js
@@ -1,43 +1,34 @@
 import { exec } from 'child_process';
 import fs from 'fs/promises';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export async function generateVideo(product, audioPath) {
   const output = `/tmp/${product.id}-video.mp4`;
+  const image = product.image;
 
-  // Create temporary list of media files for ffmpeg concat
-  const mediaListPath = `/tmp/${product.id}-medialist.txt`;
-  let mediaListContent = '';
-
-  for (const mediaUrl of product.media) {
-    const filename = `/tmp/${product.id}-${mediaUrl.split('/').pop()}`;
-    await downloadFile(mediaUrl, filename);
-    mediaListContent += `file '${filename}'\n`;
-  }
-
-  await fs.writeFile(mediaListPath, mediaListContent);
-
-  // Create slideshow/clip video
   const cmd = `
-    ffmpeg -f concat -safe 0 -i ${mediaListPath} -i ${audioPath} \
-    -filter_complex "[0:v]zoompan=z='min(zoom+0.0015,1.5)':d=125[video]" \
-    -map "[video]" -map 1:a -shortest -y ${output}
+    ffmpeg -loop 1 -i ${image} -i ${audioPath} -filter_complex "[0:v]zoompan=z='min(zoom+0.0015,1.5)':d=125[video]" -map "[video]" -map 1:a -t 30 -y ${output}
   `;
 
   await new Promise((resolve, reject) => {
     exec(cmd, (err) => (err ? reject(err) : resolve()));
   });
 
-  return uploadToCloud(output);
+  return await uploadToCloudinary(output, product.id);
 }
 
-async function downloadFile(url, outputPath) {
-  const res = await fetch(url);
-  const buffer = await res.arrayBuffer();
-  await fs.writeFile(outputPath, Buffer.from(buffer));
-}
+async function uploadToCloudinary(filePath, publicId) {
+  const res = await cloudinary.uploader.upload(filePath, {
+    resource_type: "video",
+    public_id: `trendreactor/${publicId}-${Date.now()}`,
+    overwrite: true,
+  });
 
-async function uploadToCloud(filePath) {
-  const fileBuffer = await fs.readFile(filePath);
-  const base64 = fileBuffer.toString('base64');
-  return `data:video/mp4;base64,${base64}`;
+  return res.secure_url; // This will be the video URL you send to Make.com
 }
